7f42b12ee7702fc535a41c05dda67ed03dbd4241
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-23 19:24:10 +0530

    Added code to check if two lines are equal

--- line_comparison.java
+++ line_comparison.java
@@ -4,7 +4,6 @@ public class line_comparison {
 
 	public double calculate_length(){
 		double x1,y1,x2,y2;
-		double length_of_a_line = 0;
 
 		Scanner scan = new Scanner(System.in);
 
@@ -20,19 +19,37 @@ public class line_comparison {
 		System.out.println("Enter y2 value : ");
 		y2 = scan.nextDouble();
 
-		length_of_a_line = Math.sqrt((Math.pow(x2-x1, 2)) + (Math.pow(y2-y1, 2)) );
+		double length_of_a_line = Math.sqrt((Math.pow(x2-x1, 2)) + (Math.pow(y2-y1, 2)) );
 		return Math.round(length_of_a_line*100.0)/100.0;
 	}
 
+	public void isequals(double line1, double line2){
+		String first=Double.toString(line1);
+		String second=Double.toString(line2);
+
+		boolean result = first.equals(second);
+		if(result)
+			System.out.println("Both lines are equal.");
+		else
+			System.out.println("Both lines are not equal.");
+
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program using OOPs");
 
 		line_comparison lc = new line_comparison();
 
-		System.out.println("Enter Line Co-ordinates : ");
-		double line_length = lc.calculate_length();
-		System.out.println("Length of a line is : " + line_length);
+		System.out.println("Enter Line 1 Co-ordinates : ");
+		double line_length1 = lc.calculate_length();
+		System.out.println("Length of a line 1 is : " + line_length1);
+
+
+		System.out.println("Enter Line 2 Co-ordinates : ");
+		double line_length2 = lc.calculate_length();
+		System.out.println("Length of a line 2 is : " + line_length2);
+
+		lc.isequals(line_length1,line_length2);
 
 	}
 }
